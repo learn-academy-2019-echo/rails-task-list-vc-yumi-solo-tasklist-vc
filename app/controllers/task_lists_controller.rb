b5f316af355c9fd55a643e02dc53495a4d5defad
Matthew Clark <winescout@gmail.com>
2019-10-30 15:56:16 -0700

    adds root route

--- app/controllers/task_lists_controller.rb
+++ app/controllers/task_lists_controller.rb
@@ -1,4 +1,8 @@
 class TaskListsController < ApplicationController
+  def index
+    @task_lists = TaskList.all
+  end
+
   def show
     @task_list = TaskList.find params[:id]
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #
-  get "/tasks/:id", to: 'tasks#show', as: 'task'
-  get "/task_lists/:id", to: 'task_lists#show', as: 'task_list'
+  get '/tasks/:id' => 'tasks#show', as: 'task'
+  get '/task_lists/:id' => 'task_lists#show', as: 'task_list'
+  get '/task_lists' => 'task_lists#index', as: 'task_lists'
+
+  root to: 'task_lists#index'
 end
