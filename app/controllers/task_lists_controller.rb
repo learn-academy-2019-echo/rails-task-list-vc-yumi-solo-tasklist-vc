7ecb5e53fa74b3b0afe116e27ff2a6a2e760ce28
Matthew Clark <winescout@gmail.com>
2019-10-30 15:36:06 -0700

    show for task list

---
+++ app/controllers/task_lists_controller.rb
@@ -0,0 +1,5 @@
+class TaskListsController < ApplicationController
+  def show
+    @task_list = TaskList.find params[:id]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #
-  get "/tasks/:id", to: 'tasks#show'
+  get "/tasks/:id", to: 'tasks#show', as: 'task'
+  get "/task_lists/:id", to: 'task_lists#show', as: 'task_list'
 end
